c3dd8a72c1006764ee5995721ffcd34d516ad9f9
amLarz <larzroco@gmail.com>
2026-08-24 23:19:51 +0800

    refactor: change in nlp model
    style: ruff formatting

--- data/ranker.py
+++ data/ranker.py
@@ -2,17 +2,21 @@ from db import fetch_words
 
 get_top_words = fetch_words()
 
+
 def extract_fetched():
     extracted_words = []
     for word in get_top_words:
-        extracted_words.append((word[0], word[1])) # 0 contains the id and 1 contains the word itself
+        extracted_words.append(
+            (word[0], word[1])
+        )  # 0 contains the id and 1 contains the word itself
 
     return extracted_words
 
+
 def filter_top_words(extracted_words):
-    
+
     filtered_ranked_words = []
     for index, word in enumerate(extracted_words):
         filtered_ranked_words.append({"rank": index + 1, "word": word[1], "id": word[0]})
-    
+
     return filtered_ranked_words
--- nlp/text_processor.py
+++ nlp/text_processor.py
@@ -3,7 +3,7 @@ import csv
 import spacy
 
 # load the English NLP model
-nlp = spacy.load("en_core_web_lg")
+nlp = spacy.load("en_core_web_sm")
 
 
 def load_core_functional_words():
