d64914ee18242382d0e984b174219389a5b58568
amLarz <larzroco@gmail.com>
2026-09-02 14:07:19 +0800

    test on freq_score

--- data/stat_scoring.py
+++ data/stat_scoring.py
@@ -1,14 +1,14 @@
 from data.db import fetch_words
+import math
 
+# frequency function for stat scoring, parameters of unpacked words table
+def freq_score(id, word, frequency_score, count):
+    token_frequency_score = math.log10(count + 1)  # calculate frequency score
 
-def freq_score():
-    words = fetch_words()
-    print(words)
     
     
+# main function for stat scoring, parameters of packed words table
 def stat_scoring(table):
-    for item in table:
-        
-        
-        
-    freq_score()
+    for row in table:
+        # calculate frequency score
+        freq_score(*row)  
